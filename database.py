2a597b179fc6e456102c54c653b70d3a56f9bc7b
Aravindh2920 <aravindhraman2002@gmail.com>
2026-02-23 00:57:04 +0530

    Refactor database connection parameters for improved environment variable handling

--- database.py
+++ database.py
@@ -1,14 +1,11 @@
 import psycopg2
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        host=os.getenv("PGHOST") or os.getenv("DB_HOST"),
+        user=os.getenv("PGUSER") or os.getenv("DB_USER"),
+        password=os.getenv("PGPASSWORD") or os.getenv("DB_PASSWORD"),
+        dbname=os.getenv("PGDATABASE") or os.getenv("DB_NAME"),
+        port=os.getenv("PGPORT") or os.getenv("DB_PORT")
     )
